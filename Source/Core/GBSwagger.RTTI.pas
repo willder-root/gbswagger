fade92da10a54b12d1e68c76a2eaee8a64dfd4ed
willder-root <willderaraujo_99_gds@hotmail.com>
2026-02-28 14:11:09 -0300

    Fix of case definition for property name in SwagRequired

--- Source/Core/GBSwagger.RTTI.pas
+++ Source/Core/GBSwagger.RTTI.pas
@@ -1,4 +1,4 @@
-unit GBSwagger.RTTI;
+ï»¿unit GBSwagger.RTTI;
 
 interface
 
@@ -57,8 +57,8 @@ type
     function IsTime: Boolean;
     function IsBoolean: Boolean;
 
-    // Vários ORMs trabalham com tipo Nullable
-    // Colaboração do Giorgio para essa compatibilidade
+    // Vï¿½rios ORMs trabalham com tipo Nullable
+    // Colaboraï¿½ï¿½o do Giorgio para essa compatibilidade
     function IsNullable: Boolean;
     function NullableType: string;
 
@@ -779,7 +779,10 @@ begin
     if (Assigned(LSwaggerRequired)) then
     begin
       SetLength(Result, Length(Result) + 1);
-      Result[Length(Result) - 1] := LProp.Name;
+      case TGBSwaggerModelConfig.GetInstance(nil).CaseDefinition of
+        cdLower: Result[Length(Result) - 1]  := LProp.Name.ToLower;
+        cdUpper: Result[Length(Result) - 1]  := LProp.Name.ToUpper;
+      end;
       Continue;
     end;
 
